a82d9db23735266f7b5f8e256b95427821f2e6c8
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-09 15:14:38 +0200

    Initial Commit

---
+++ Covid19/Covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19App.swift
+//  Covid19
+//
+//  Created by AbdelrahmanQasim on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
